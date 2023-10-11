e3cfa294dbac04520599350e053867c425816345
FerEspinal22 <ferespinal2001@gmail.com>
2023-10-10 22:58:21 -0600

    Avance hasta el video 197

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+
+import { Observable, catchError, of } from 'rxjs';
+
 import { environments } from 'src/environments/environments';
+
 import { Hero } from '../interfaces/hero.interface';
 
 @Injectable({providedIn: 'root'})
@@ -13,6 +16,13 @@ export class HeroesService {
 
   getHeroes():Observable<Hero[]> {
     return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`);
+  }
+
+  getHeroById(id: string): Observable<Hero | undefined> {
+    return this.http.get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
+      .pipe(
+        catchError( error => of(undefined) )
+      );
   }
 
 }
